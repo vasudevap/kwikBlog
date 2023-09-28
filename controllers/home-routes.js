41b5c16dd473645eaaf421f23a48766d84fb12f8
PV <140992905+vasudevap@users.noreply.github.com>
2023-09-28 19:48:26 -0400

    ADDED route GET for root to get all blogposts

---
+++ controllers/home-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const path = require('path');
+
+const { User, BlogPost } = require('../models');
+
+// GET All BlogPosts
+router.get('/', async (req, res) => {
+    // Get all BlogPosts and JOIN with user data
+    BlogPost.findAll()
+        .then((blogPosts) => {
+            // Serialize data so the template can read it
+            const blogPost = blogPosts.map((bp) => bp.get({ plain: true }));
+            res.status(200).json(blogPost);
+        })
+        .catch((err) => res.status(400).json(err))
+});
